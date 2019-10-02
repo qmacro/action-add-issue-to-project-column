b6e7712af2a5cfbef79b0cc11a97be350ed775b0
DJ Adams <dj.adams@sap.com>
2019-10-02 10:55:36 +0100

    modify stringify output

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function run() {
   try {
     const repo = core.getInput('repo')
     console.log(`Repo '${repo}' specified`)
-    console.log(`Context is '${JSON.stringify(github.context)}'`)
+    console.log(`Context is '${JSON.stringify(github.context, undefined, 2)}'`)
     core.setOutput('projects', 'a,b,c')
   }
   catch (error) {
