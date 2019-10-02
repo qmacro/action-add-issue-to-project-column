d18183d35051bd1db9d6f168652087b8a16eb588
DJ Adams <dj.adams@sap.com>
2019-10-02 12:07:26 +0100

    err

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 async function run() {
-  try {
-    const token = core.getInput('myToken')
-    console.log(`Token: ${token.slice(0,4)}... (total length ${token.length})`)
-    console.log(`Context is '${JSON.stringify(github.context, undefined, 2)}'`)
-    core.setOutput('projects', 'a,b,c')
-  }
-  catch (error) {
-    core.setFailed(error.message)
-  }
+	try {
+		console.log(`You sent ${core.getInput('project')} / ${core.getInput('column')}`)
+
+		const octokit = new github.GitHub(core.getInput('myToken'))
+
+		const data = await octokit.projects.listForRepo({
+			owner: github.context.payload.repository.owner.login,
+			repo: github.context.payload.repository.name
+		})
+
+		const projectNames = data.map(x => x.name)
+
+		core.setOutput('projects', JSON.stringify(projectnames))
+	}
+	catch (error) {
+		core.setFailed(error.message)
+	}
 }
 
 run()
